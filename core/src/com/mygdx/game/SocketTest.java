055a7349b3c87893a3d8060489d81532d1ed958a
Oliver Dixon <olly257@gmail.com>
2015-09-15 16:23:17 +0200

    .gitignore is now working

--- core/src/com/mygdx/game/SocketTest.java
+++ core/src/com/mygdx/game/SocketTest.java
@@ -3,25 +3,77 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
 
-public class SocketTest extends ApplicationAdapter {
+public class SocketTest extends ApplicationAdapter
+{
 	SpriteBatch batch;
-	Texture img;
-	
+
 	@Override
-	public void create () {
+	public void create ()
+    {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+        Gdx.input.setInputProcessor(new GestureDetector(new GestureDetector.GestureListener() {
+            @Override
+            public boolean touchDown(float x, float y, int pointer, int button) {
+                return false;
+            }
+
+            @Override
+            public boolean tap(float x, float y, int count, int button) {
+
+                log("Beginning connection with SocketIO server.");
+
+
+
+                return false;
+            }
+
+            @Override
+            public boolean longPress(float x, float y) {
+                return false;
+            }
+
+            @Override
+            public boolean fling(float velocityX, float velocityY, int button) {
+                return false;
+            }
+
+            @Override
+            public boolean pan(float x, float y, float deltaX, float deltaY) {
+                return false;
+            }
+
+            @Override
+            public boolean panStop(float x, float y, int pointer, int button) {
+                return false;
+            }
+
+            @Override
+            public boolean zoom(float initialDistance, float distance) {
+                return false;
+            }
+
+            @Override
+            public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+                return false;
+            }
+        }));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
 	}
+
+    static void log(Object object)
+    {
+        System.out.println("LOG_LIBGDX: " + object);
+    }
 }
